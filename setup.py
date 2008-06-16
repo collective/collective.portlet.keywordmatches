33f0f9cac199cb31ab8834ca8ab0f28fec0d3b15
Clayton Parker <me@claytron.com>
2008-06-16 14:11:40 +0000

    collective.portlet.keywordmatches version 1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.6'
+version = '1.0'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
